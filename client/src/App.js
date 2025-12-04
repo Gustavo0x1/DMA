eca3818db1f7051dacc1ed69240676d857831639
Gustavo <gustavosrd4@gmail.com>
2025-12-04 01:33:04 -0300

    Working snap to grid and add image to the grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,13 @@
 
 import './App.css';
-import io from 'socket.io-client'
-import {useEffect,useState} from "react"
-import RPGGrid from './components/GRID/RPGGrid';
+import { useEffect, useState } from "react"
+import GameMaster from './components/GRID/GameMaster';
 
 
-const socket = io.connect("http://localhost:3001")
 function App() {
-  const [message,setMessage] = useState("")
-  const [MessageReceived,setMessageReceived] = useState("")
-
-  const senMessage = () =>{
-
-    socket.emit("send_message",{message})
-    
-  }
-
-  useEffect(()=>{
-
-    socket.on("receive_message",(data)=>{
-
-      setMessageReceived(data.message)
-
-    })
-
-  },[socket])
-
   return (
     <div className="App">
-      
-      <RPGGrid></RPGGrid>
- 
-      <input type='text' onChange={(event)=>{setMessage(event.target.value)}} placeholder='message'></input>
-      <button onClick={senMessage}>Send message</button>
-      <h1>Data 1 {MessageReceived}</h1>
+      <GameMaster />
     </div>
   );
 }
